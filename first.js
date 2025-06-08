272a36e8230afaad9890c20abc09906ed02bfc4d
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-06-08 21:01:23 +0530

    files are linked

---
+++ first.js
@@ -0,0 +1,8 @@
+// console.log("Welcome to Apna college");
+// console.log("Pratik Salunke");
+// console.log("I love Java Script");
+
+fullname = "Pratik Salunke";
+console.log(fullname);
+age="20";
+console.log(age)
