90b4127ab8b64c08dce17875f49a67048ce908e7
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-06-10 09:29:37 +0530

    Primitive Data types

--- first.js
+++ first.js
@@ -1,8 +1,66 @@
 // console.log("Welcome to Apna college");
-// console.log("Pratik Salunke");
-// console.log("I love Java Script");
+// // console.log("Pratik Salunke");
+// // console.log("I love Java Script");
 
-fullname = "Pratik Salunke";
-console.log(fullname);
-age="20";
-console.log(age)
+// // fullname = "Pratik Salunke";
+// // console.log(fullname);
+// // age="20";
+// // console.log(age)
+// isFollow="true";
+// console.log(isFollow);
+
+// fullname = "Pratik Salunke";
+// // fullname = 24;
+// // console.log(fullname);
+// let fullName="Pratik Salunke";
+// // console.log(fullName);
+
+// const PI=3.14;
+// console.log(PI);
+
+
+// {
+//     let a=5;
+//     console.log(a);
+
+// }
+// {
+//     let a=10;
+//     console.log(a);
+
+// }
+
+
+// // __Data Types in __
+// // Primitive Types : Number, String, Boolean, Undefined, Null, BigInt, Symbol
+
+// Number
+// let age= 10;
+//  ..check typeof at console
+// let price=455.25;
+//  ..check typeof at console
+
+// String
+// let fullName="Pratik Salunke";
+// ..check typeof at console
+
+// Boolean
+// isFollow="true";
+// ..check typeof at console
+
+// Undefined
+// let x;
+// ..check typeof at console
+
+// Null
+// let x=null;
+// ..check typeof at console
+
+// Bigint
+// let x = BigInt("764");
+// ..check typeof at console
+
+// Symbol
+// let y = Symbol("Hellow!");
+// ..check typeof at console
+
