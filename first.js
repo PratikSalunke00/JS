6ac3aeb91bc41b56ad25098bcf7a9ff6d79a0758
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-06-10 12:28:56 +0530

    Lecture 1 commplete

--- first.js
+++ first.js
@@ -64,3 +64,37 @@
 // let y = Symbol("Hellow!");
 // ..check typeof at console
 
+// __Non Prrimitive Data type
+// const student={
+//     fullName:"Pratik Salunke",
+//     age:20,
+//     cgpa:8.2,
+//     isPass: true,
+// };
+// student["age"]=student["age"]+1;
+// // console.log(student ["age"]);
+// console.log(student.age);
+
+
+//  Qs1. Create a const object called “product” to store information shown in the picture.
+// const prodduct={
+//     name:"Ball Pen",
+//     rating:4,
+//     offer:5,
+//     price:260,
+// };
+// console.log(prodduct);
+
+// Qs2. Create a const object called “profile” to store information shown in the picture.
+// const profile={
+//     username:"@pratiksalunke682",
+//     isFollow:false,
+//     followers:74,
+//     following:324,
+// };
+// console.log( profile);
+// console.log(typeof profile);
+// console.log(typeof profile.username);
+// console.log(typeof profile.isFollow);
+// console.log(typeof profile.following);
+// console.log(profile.username);
