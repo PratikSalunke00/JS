ca5fe8bf1811d6390c096a94924bebe6b8a7130c
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-07-19 13:13:16 +0530

    Template liteerals

---
+++ script.js
@@ -0,0 +1,44 @@
+// __ Operators in JS__
+// Arithmattic Operators
+// let a=5;
+// let b=2;
+// console.log("a=",a , "& b =" ,b );
+// console.log("a+b=" , a+b);
+// console.log("a-b=" , a-b);
+// console.log("a*b=" , a*b);
+// console.log("a/b=" , a/b);
+// console.log("a%b=" , a%b);
+// console.log("a**b=" , a**b);
+
+// Unary Operator
+// let a=5;
+// let b=2;
+// console.log("a=",a , "& b =" ,b );
+// // a++;
+// a--;
+// console.log("a =" , a);
+// console.log("a++=" , a++); //5
+// console.log("a=" , a);//6
+// console.log("a--=" , a--); 
+// console.log("a=" , a);
+// console.log("--a=" , --a); 
+// console.log("a=" , a);
+// console.log("++a=" , ++a); 
+// console.log("a=" , a);
+
+//  Assignment Operators
+//  = +=-= *= %= **=
+// let a=5;
+// let b=2;
+// a+=4;//a=a+4
+// console.log("a=",a);//9
+// a-=4;//a=a-4
+// console.log("a=",a);
+// a*=4;//a=a*4
+// console.log("a=",a);
+// a/=4;//a=a/4
+// console.log("a=",a);
+// a%=4;//a=a%4
+// console.log("a=",a);
+// a**=4;//a=a**4
+// console.log("a=",a);
---
+++ second.js
@@ -0,0 +1,73 @@
+    //** Template Literals **
+// let a=10;
+// let b=5;
+// // let output="The addition is :" + (a+b) + "Rupees.";
+// let output=`The addition is : ${a+b} Rupees.`;
+// console.log(output);
+    //... ... ... ... ... ... ... ... ... ... ... ... ...
+
+
+    //**** Operators in JS ****
+
+    //** Arithmattic Operators **
+// let a=5;
+// let b=2;
+// console.log("a=",a , "& b =" ,b );
+// console.log("a+b=" , a+b);
+// console.log("a-b=" , a-b);
+// console.log("a*b=" , a*b);
+// console.log("a/b=" , a/b);
+// console.log("a%b=" , a%b);
+// console.log("a**b=" , a**b);
+    //... ... ... ... ... ... ... ... ... ... ... ... ...
+
+    //** Unary Operator **
+// let a=5;
+// let b=2;
+// console.log("a=",a , "& b =" ,b );
+// a++;
+// // a--;
+// console.log("a =" , a);
+
+// console.log("a++=" , a++);
+// console.log("a=" , a);
+
+// console.log("a--=" , a--); 
+// console.log("a=" , a);
+
+// console.log("--a=" , --a); 
+// console.log("a=" , a);
+
+// console.log("++a=" , ++a); 
+// console.log("a=" , a);
+    //... ... ... ... ... ... ... ... ... ... ... ... ...
+
+    //** Assignment Operators **
+//  = +=-= *= %= **=
+// let a=5;
+// let b=2;
+// a+=4;//a=a+4
+// console.log("a=",a);//9
+// a-=4;//a=a-4
+// console.log("a=",a);
+// a*=4;//a=a*4
+// console.log("a=",a);
+// a/=4;//a=a/4
+// console.log("a=",a);
+// a%=4;//a=a%4
+// console.log("a=",a);
+// a**=4;//a=a**4
+// console.log("a=",a);
+    //... ... ... ... ... ... ... ... ... ... ... ... ...
+
+    //**  Comaparison Operators **
+// > >= < <= == !=
+// let age =18;
+// console.log(age>18);
+// console.log(age>=18);
+// console.log(age<18);
+// console.log(age<=18);
+// console.log(age==18);
+// console.log(age!=18);
+    //... ... ... ... ... ... ... ... ... ... ... ... ...
+
