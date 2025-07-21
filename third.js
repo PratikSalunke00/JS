ba9f2f43505dbe32e41c0466670a9ef658de08e5
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-07-21 22:46:34 +0530

    Switch statement learned

--- third.js
+++ third.js
@@ -179,3 +179,51 @@
 //     console.log("Pass");
 // }
 // ... ... ... ... ... ... ... ... ... ... ... ... ...
+
+
+//** switch Statement **
+// let color="red";
+// switch(color){
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "yellow":
+//         console.log("slow down");
+//         break;
+//     case "green":
+//         console.log("go");
+//         break;
+//     default:
+//         console.log("Light is broken");
+// }
+// console.log("after switch stt");
+// ... ... ... ... ... ... ... ... ... ... ... ... ...
+
+//** Practice Quetion **
+// let day = 1;
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Wrong day !");
+// }
+// ... ... ... ... ... ... ... ... ... ... ... ... ...
