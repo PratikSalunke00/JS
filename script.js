078f992a6eb4a58411a518a614ad4bf9b3602938
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-07-24 22:05:46 +0530

    for loop learned

--- script.js
+++ script.js
@@ -447,3 +447,49 @@
 // console.log(game[0]);
 // console.log(game);
 // ... ... ... ... ... ... ... ... ... ... ... ... ...
+
+
+//**** Loops ****
+
+//** for loop **
+// for(let i=1; i<=5; i++){
+//     console.log(i);
+// }
+// for(let i=10; i>=1; i=i-3){
+//     console.log(i);
+// }
+// ... ... ... ... ... ... ... ... ... ... ... ... ...
+
+//** Practice Quetion **
+// To print all odd numbers (1 to 15)
+// for(let i=1;i<=15;i+=2){
+//     console.log(i);
+// }
+// console.log("backwords loop");
+// for(let i=15;i>=1;i-=2){
+//     console.log(i);
+// }
+// ... ... ... ... ... ... ... ... ... ... ... ... ...
+
+//** Practice Quetion **
+// To print all even numbers (2 to 10)
+// for(let i=2;i<=10;i+=2){
+//     console.log(i);
+// }
+// console.log("backwords loop");
+// for(let i=10;i>=2;i-=2){
+//     console.log(i);
+// }
+// ... ... ... ... ... ... ... ... ... ... ... ... ...
+
+//** Infinite loops **
+// for(let i=1;i>=0;i++){
+//     console.log(i);
+// }
+// for(let i=1;i<=5;i--){
+//     console.log(i);
+// }
+// for(let i=1;i++){
+//     console.log(i);
+// }
+// ... ... ... ... ... ... ... ... ... ... ... ... ...
