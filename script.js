6531c31f01ad13a15660bfe1c15df0ac5fd9412e
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-09-05 21:05:40 +0530

    Extracting Form data

--- script.js
+++ script.js
@@ -1726,3 +1726,28 @@
 //     alert("form submitted");
 // });
 // ... ... ... ... ... ... ... ... ... ... ... ...  ...
+
+
+//*** Extracting Form data ***
+// let form = document.querySelector("form");
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     // let user = document.querySelector("#user");
+//     // let pass = document.querySelector("#pass");
+//     // // console.log(user);
+//     // // console.log(pass);
+//     // // console.dir(user);
+//     // // console.dir(pass);
+//     // console.log(user.value);
+//     // console.log(pass.value);
+//     // alert(`Hi ${user.value}, your password is set to ${pass.value}`);
+
+//     console.dir(form);
+//     let user = this.elements[0];  //form.elements[0]
+//     let pass = this.elements[1];
+//     console.log(user.value);
+//     console.log(pass.value);
+//     alert(`Hi ${user.value}, your password is set to ${pass.value}`);
+// });
+// ... ... ... ... ... ... ... ... ... ... ... ...  ...
