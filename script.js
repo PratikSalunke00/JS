4f42a966b68c33251f0f52e68df41e030c550d2f
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-09-05 14:27:55 +0530

    Practice question on Keyboard Event

--- script.js
+++ script.js
@@ -1683,3 +1683,39 @@
 //     console.log("button clicked");
 // });
 // ... ... ... ... ... ... ... ... ... ... ... ...  ...
+
+//* Keyboard Event *
+// let inp = document.querySelector("input");
+// inp.addEventListener("keydown", function(event) {
+//     console.log(event);
+//     console.log("key was pressed");
+// });
+
+// inp.addEventListener("keydown", function(event) {
+//     console.log("key=", event.key);
+//     console.log("code=", event.code);
+//     console.log("key was pressed");
+// });
+
+// let inp = document.querySelector("input");
+// inp.addEventListener("keyup", function() {
+//     console.log("key was released");
+// });
+// ... ... ... ... ... ... ... ... ... ... ... ...  ...
+
+
+//** Practice Quetions **
+// let inp = document.querySelector("input");
+// inp.addEventListener("keydown", function(event){
+//     console.log("code=", event.code);  //ArrowUp, ArrowDown, ArrowLeft, ArrowRight
+//     if (event.code == "ArrowUp"){
+//         console.log("character moves forward");
+//     } else if (event.code == "ArrowDown"){
+//         console.log("character moves backward");
+//     } else if (event.code == "ArrowLeft"){
+//         console.log("character moves Left");
+//     } else if (event.code == "ArrowRight"){
+//         console.log("character moves Right");
+//     }
+// });
+// ... ... ... ... ... ... ... ... ... ... ... ...  ...
